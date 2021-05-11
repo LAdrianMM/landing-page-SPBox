a55ecd2da89bcbdc61692c407585d51d5ca2fbfb
LAdrianMM <ladrianmm@gmail.com>
2021-05-11 12:11:19 -0400

    update bakend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') // Permite leer los datos enviados en un metodo POST por ejemplo
 const cors = require('cors')
 
 app.use(cors())
-app.use(bodyParser.json())
+app.use(bodyParser.json()) // En esta linea permite leer los datos en formato json, actualmente se usa "express.json()"
 
 // Route
 const Mail = require('./routes/mail')
